27c9b95e44481cbd5b3039fb3867ed09ae0c2a63
Al Ch <32143756+OrangeRoof@users.noreply.github.com>
2019-01-20 11:49:35 -0800

    finished code

--- arduinoCode/muscleToAccel/muscleToAccel.ino
+++ arduinoCode/muscleToAccel/muscleToAccel.ino
@@ -9,89 +9,92 @@ const long BTPeriod = 10; // millisec
 const int LEFT = 4;
 const int RIGHT = 2;
 char movement;
-int buffer = 25;
+int buffer = 75;
+unsigned long recTime;
+double sensorValue;
+bool FLEX;
 
 /******************** SETUP ********************/
 void setup(){
-  BT.begin(BTBaudRate);
-  
-  Serial.begin(9600);                 // Start the serial terminal
-  Serial.println("Hello World!");
-  Serial.println();
+	BT.begin(BTBaudRate);
 
-  adxl.powerOn();                     // Power on the ADXL345
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  adxl.setRangeSetting(2);           // Give the range settings
-  // Accepted values are 2g, 4g, 8g or 16g
-  // Higher Values = Wider Measurement Range
-  // Lower Values = Greater Sensitivity
-  adxl.setSpiBit(0);                  // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
-                                      // Default: Set to 1
-                                      // SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library
+	adxl.powerOn();                     // Power on the ADXL345
 
-  adxl.setActivityXYZ(1, 0, 0);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
-  adxl.setActivityThreshold(75);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+	adxl.setRangeSetting(2);           // Give the range settings
+	// Accepted values are 2g, 4g, 8g or 16g
+	// Higher Values = Wider Measurement Range
+	// Lower Values = Greater Sensitivity
+	adxl.setSpiBit(0);                  // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
+	// Default: Set to 1
+	// SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library
 
-  adxl.setInactivityXYZ(1, 0, 0);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
-  adxl.setInactivityThreshold(75);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
-  adxl.setTimeInactivity(10);         // How many seconds of no activity is inactive?
+	adxl.setActivityXYZ(1, 0, 0);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+	adxl.setActivityThreshold(75);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
 
-  adxl.setTapDetectionOnXYZ(0, 0, 1); // Detect taps in the directions turned ON "adxl.setTapDetectionOnX(X, Y, Z);" (1 == ON, 0 == OFF)
+	adxl.setInactivityXYZ(1, 0, 0);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+	adxl.setInactivityThreshold(75);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+	adxl.setTimeInactivity(10);         // How many seconds of no activity is inactive?
 
-  // Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
-  adxl.setTapThreshold(50);           // 62.5 mg per increment
-  adxl.setTapDuration(15);            // 625 μs per increment
-  adxl.setDoubleTapLatency(80);       // 1.25 ms per increment
-  adxl.setDoubleTapWindow(200);       // 1.25 ms per increment
+	adxl.setTapDetectionOnXYZ(0, 0, 1); // Detect taps in the directions turned ON "adxl.setTapDetectionOnX(X, Y, Z);" (1 == ON, 0 == OFF)
 
-  // Set values for what is considered FREE FALL (0-255)
-  adxl.setFreeFallThreshold(7);       // (5 - 9) recommended - 62.5mg per increment
-  adxl.setFreeFallDuration(30);       // (20 - 70) recommended - 5ms per increment
+	// Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
+	adxl.setTapThreshold(50);           // 62.5 mg per increment
+	adxl.setTapDuration(15);            // 625 μs per increment
+	adxl.setDoubleTapLatency(80);       // 1.25 ms per increment
+	adxl.setDoubleTapWindow(200);       // 1.25 ms per increment
 
-  // Setting all interupts to take place on INT1 pin
-  //adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);"
-  // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
-  // This library may have a problem using INT2 pin. Default to INT1 pin.
+	// Set values for what is considered FREE FALL (0-255)
+	adxl.setFreeFallThreshold(7);       // (5 - 9) recommended - 62.5mg per increment
+	adxl.setFreeFallDuration(30);       // (20 - 70) recommended - 5ms per increment
 
-  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
-  adxl.InactivityINT(1);
-  adxl.ActivityINT(1);
-  adxl.FreeFallINT(1);
-  adxl.doubleTapINT(1);
-  adxl.singleTapINT(1);
+	// Setting all interupts to take place on INT1 pin
+	//adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);"
+	// Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+	// This library may have a problem using INT2 pin. Default to INT1 pin.
 
-  //attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+	// Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+	adxl.InactivityINT(1);
+	adxl.ActivityINT(1);
+	adxl.FreeFallINT(1);
+	adxl.doubleTapINT(1);
+	adxl.singleTapINT(1);
 
+	//attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
 }
 
 void loop() {
-  // EGM Muscle Readings
-  double sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+	delay(100);
+	// EGM Muscle Readings
+	sensorValue = analogRead(A0);
+	digitalWrite(LED_BUILTIN, (FLEX ? HIGH : LOW));
 
-  // Accelerometer Readings
-  int x,y,z;
-  adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
+	// Accelerometer Readings
+	int x,y,z;
+	adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
+	
+	if(sensorValue >= 550 && (millis() - recTime) > 1000){
+		FLEX = !FLEX;
+		recTime = millis();
+	}
 
-  // checking if muscle was flexed
-  if(sensorValue >= 800 && !BT.available()) {
-    if((-300+buffer > z && -300-buffer <= z) && (buffer > y && -1*buffer <= y)) {
-      movement = 'd';
-    }
-    else if((-300+buffer > z && -300-buffer <= z) && (200+buffer > y && 200-buffer <= y)) {
-      movement = 'a';
-    }
-    else if((200+buffer > x && 200-buffer < x) && (buffer > z && -1*buffer < z)) {
-      movement = 'w';
-    }
-    else if((-300+buffer > x && 300-buffer < x) && (buffer > z && -1*buffer < z)) {
-      movement = 's';
-    }
-  } else {
-    movement = 'a';
-    movement = BT.write(movement);
-    Serial.println(movement);
-  }
+	Serial.println(millis() - recTime);
 
-  delay(100);
+	if(!FLEX)
+		return;
+
+	if(abs(150 - x) < buffer){
+		BT.write('w');
+		Serial.println('w');
+	}
+	if(abs(200 - y) < buffer) {
+		BT.write('a');
+		Serial.println('a');
+	}
+	if(abs(-200 - y) < buffer) {
+		BT.write('d');
+		Serial.println('d');
+	}
+
 }
